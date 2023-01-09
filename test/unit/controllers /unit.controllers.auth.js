0f08b88b79d9d97a5e8e5a978c1374f537fc2a42
Rashidat Sikiru <rashidatsikiru@Rashidats-MacBook-Pro.local>
2023-01-09 16:20:36 +0100

    fixed part of the unit tests

--- test/unit/controllers /unit.controllers.auth.js
+++ test/unit/controllers /unit.controllers.auth.js
@@ -29,116 +29,33 @@ describe('', () => {
     });
   });
 
-});
-
-describe('', () => {
-  let status,
-    next;
-
-  const res = {
-    status: 'error',
-    error: 'INTERNAL_SERVER_ERROR',
-    code: enums.HTTP_INTERNAL_SERVER_ERROR
-  };
-
-  beforeEach(() => {
-    status = sinon.stub();
-    next = sinon.stub();
-    status.returns(res);
-    next.returns(res);
+  it('should call verify user email', async() => {
+    const req = { user: undefined };
+    await AuthController.verifyEmail(req, res, next);
+    expect(res.code).to.equal(500);
+    expect(res.error).to.equal('INTERNAL_SERVER_ERROR');
   });
 
-  describe('User controller catch block unit testings', () => {
-    it('should call verify user email', async() => {
-      const req = { user: undefined };
-      await AuthController.verifyEmail(req, res, next);
-      expect(res.code).to.equal(500);
-      expect(res.error).to.equal('INTERNAL_SERVER_ERROR');
-    });
+  it('should call user login', async() => {
+    const req = { user: undefined };
+    await AuthController.loginClient(req, res, next);
+    expect(res.code).to.equal(500);
+    expect(res.error).to.equal('INTERNAL_SERVER_ERROR');
   });
 
-});
-
-describe('', () => {
-  let status,
-    next;
-
-  const res = {
-    status: 'error',
-    error: 'INTERNAL_SERVER_ERROR',
-    code: enums.HTTP_INTERNAL_SERVER_ERROR
-  };
-
-  beforeEach(() => {
-    status = sinon.stub();
-    next = sinon.stub();
-    status.returns(res);
-    next.returns(res);
+  it('should call user forgot password', async() => {
+    const req = { forgot_password_token: undefined, body: undefined, expTime: undefined };
+    await AuthController.forgotPassword(req, res, next);
+    expect(res.code).to.equal(500);
+    expect(res.error).to.equal('INTERNAL_SERVER_ERROR');
   });
 
-  describe('User controller catch block unit testings', () => {
-    it('should call user login', async() => {
-      const req = { user: undefined };
-      await AuthController.loginClient(req, res, next);
-      expect(res.code).to.equal(500);
-      expect(res.error).to.equal('INTERNAL_SERVER_ERROR');
-    });
+  it('should call user reset password', async() => {
+    const req = { hashedPassword: undefined, user: undefined };
+    await AuthController.resetPassword(req, res, next);
+    expect(res.code).to.equal(500);
+    expect(res.error).to.equal('INTERNAL_SERVER_ERROR');
   });
 
 });
 
-describe('', () => {
-  let status,
-    next;
-
-  const res = {
-    status: 'error',
-    error: 'INTERNAL_SERVER_ERROR',
-    code: enums.HTTP_INTERNAL_SERVER_ERROR
-  };
-
-  beforeEach(() => {
-    status = sinon.stub();
-    next = sinon.stub();
-    status.returns(res);
-    next.returns(res);
-  });
-
-  describe('User controller catch block unit testings', () => {
-    it('should call user login', async() => {
-      const req = { forgot_password_token: undefined, body: undefined, expTime: undefined };
-      await AuthController.forgotPassword(req, res, next);
-      expect(res.code).to.equal(500);
-      expect(res.error).to.equal('INTERNAL_SERVER_ERROR');
-    });
-  });
-
-});
-
-describe('', () => {
-  let status,
-    next;
-
-  const res = {
-    status: 'error',
-    error: 'INTERNAL_SERVER_ERROR',
-    code: enums.HTTP_INTERNAL_SERVER_ERROR
-  };
-
-  beforeEach(() => {
-    status = sinon.stub();
-    next = sinon.stub();
-    status.returns(res);
-    next.returns(res);
-  });
-
-  describe('User controller catch block unit testings', () => {
-    it('should call user login', async() => {
-      const req = { hashedPassword: undefined, user: undefined };
-      await AuthController.resetPassword(req, res, next);
-      expect(res.code).to.equal(500);
-      expect(res.error).to.equal('INTERNAL_SERVER_ERROR');
-    });
-  });
-
-});
